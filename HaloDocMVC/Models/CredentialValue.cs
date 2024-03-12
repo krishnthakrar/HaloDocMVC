05c6f77e4c0f972265dd9cb3ea007ba5357c1166
krishnthakrar <krishn.thakrar@etatvasoft.com>
2024-03-12 11:18:26 +0530

    12-03 Commit

--- HaloDocMVC/Models/CredentialValue.cs
+++ HaloDocMVC/Models/CredentialValue.cs
@@ -5,107 +5,69 @@ namespace HaloDocMVC.Models
     public static class CredentialValue
     {
         private static IHttpContextAccessor _httpContextAccessor;
+
         static CredentialValue()
         {
             _httpContextAccessor = new HttpContextAccessor();
         }
-
-        public class DecodedToken
-        {
-            private List<string> audience1;
-            private List<(string Type, string Value)> claims1;
-            private DateTime validTo;
-            private string signatureAlgorithm;
-            private string rawData;
-            private string subject;
-            private DateTime validFrom;
-            private string encodedHeader;
-            private string encodedPayload;
-
-            public DecodedToken(string keyId, string issuer, List<string> audience1, List<(string Type, string Value)> claims1, DateTime validTo, string signatureAlgorithm, string rawData, string subject, DateTime validFrom, string encodedHeader, string encodedPayload)
-            {
-                this.keyId = keyId;
-                Issuer = issuer;
-                this.audience1 = audience1;
-                this.claims1 = claims1;
-                this.validTo = validTo;
-                this.signatureAlgorithm = signatureAlgorithm;
-                this.rawData = rawData;
-                this.subject = subject;
-                this.validFrom = validFrom;
-                this.encodedHeader = encodedHeader;
-                this.encodedPayload = encodedPayload;
-            }
-
-            public string? keyId { get; set; }
-            public string? Issuer { get; set; }
-            public string? audience { get; set; }
-            public string? claims { get; set; }
-            public string? ValidTo { get; set; }
-            public string? SignatureAlgorithm { get; set; }
-            public string? RawData { get; set; }
-            public string? Subject { get; set; }
-            public string? ValidFrom { get; set; }
-            public string? EncodedHeader { get; set; }
-            public string? EncodedPayloa { get; set; }
-        }
         public static string? role()
         {
-            string cookieValue = _httpContextAccessor.HttpContext.Request.Cookies["jwt"];
-            var handler = new JwtSecurityTokenHandler();
-            var jsonToken = handler.ReadToken(cookieValue);
-            var tokenS = jsonToken as JwtSecurityToken;
+            string cookieValue;
+            string role = null;
 
-            var d = ConvertJwtStringToJwtSecurityToken(cookieValue);
-            var dd = DecodeJwt(d);
-            var jti = tokenS.Claims.First(claim => claim.Type == "UserId").Value;
-            return jti;
-        }
-        public static JwtSecurityToken ConvertJwtStringToJwtSecurityToken(string? jwt)
-        {
-            var handler = new JwtSecurityTokenHandler();
-            var token = handler.ReadJwtToken(jwt);
+            if (_httpContextAccessor.HttpContext.Request.Cookies["jwt"] != null)
+            {
+                cookieValue = _httpContextAccessor.HttpContext.Request.Cookies["jwt"].ToString();
 
-            return token;
+                role = DecodedToken.DecodeJwt(DecodedToken.ConvertJwtStringToJwtSecurityToken(cookieValue)).claims.FirstOrDefault(t => t.Key == "Role").Value;
+            }
+
+            return role;
         }
 
-        public static DecodedToken DecodeJwt(JwtSecurityToken token)
-        {
-            var keyId = token.Header.Kid;
-            var audience = token.Audiences.ToList();
-            var claims = token.Claims.Select(claim => (claim.Type, claim.Value)).ToList();
 
-            return new DecodedToken(
-                keyId,
-                token.Issuer,
-                audience,
-                claims,
-                token.ValidTo,
-                token.SignatureAlgorithm,
-                token.RawData,
-                token.Subject,
-                token.ValidFrom,
-                token.EncodedHeader,
-                token.EncodedPayload
-            );
-        }
         public static string? UserName()
         {
-            string cookieValue = _httpContextAccessor.HttpContext.Request.Cookies["jwt"];
-            var handler = new JwtSecurityTokenHandler();
-            var jsonToken = handler.ReadToken(cookieValue);
-            var tokenS = jsonToken as JwtSecurityToken;
-            var UserName = tokenS.Claims.First(claim => claim.Type == "Username").Value;
+            string cookieValue;
+            string UserName = null;
+
+            if (_httpContextAccessor.HttpContext.Request.Cookies["jwt"] != null)
+            {
+                cookieValue = _httpContextAccessor.HttpContext.Request.Cookies["jwt"].ToString();
+
+                UserName = DecodedToken.DecodeJwt(DecodedToken.ConvertJwtStringToJwtSecurityToken(cookieValue)).claims.FirstOrDefault(t => t.Key == "Username").Value;
+            }
+
             return UserName;
         }
 
-        public static string? UserID()
+        public static string? UserId()
         {
-            string cookieValue = _httpContextAccessor.HttpContext.Request.Cookies["jwt"];
-            var handler = new JwtSecurityTokenHandler();
-            var jsonToken = handler.ReadToken(cookieValue);
-            var tokenS = jsonToken as JwtSecurityToken;
-            var UserID = tokenS.Claims.First(claim => claim.Type == "UserId").Value;
+            string cookieValue;
+            string UserID = null;
+
+            if (_httpContextAccessor.HttpContext.Request.Cookies["jwt"] != null)
+            {
+                cookieValue = _httpContextAccessor.HttpContext.Request.Cookies["jwt"].ToString();
+
+                UserID = DecodedToken.DecodeJwt(DecodedToken.ConvertJwtStringToJwtSecurityToken(cookieValue)).claims.FirstOrDefault(t => t.Key == "UserID").Value;
+            }
+
+            return UserID;
+        }
+
+        public static string? Id()
+        {
+            string cookieValue;
+            string UserID = null;
+
+            if (_httpContextAccessor.HttpContext.Request.Cookies["jwt"] != null)
+            {
+                cookieValue = _httpContextAccessor.HttpContext.Request.Cookies["jwt"].ToString();
+
+                UserID = DecodedToken.DecodeJwt(DecodedToken.ConvertJwtStringToJwtSecurityToken(cookieValue)).claims.FirstOrDefault(t => t.Key == "UserID").Value;
+            }
+
             return UserID;
         }
     }
---
+++ HaloDocMVC/Models/DecodedToken.cs
@@ -0,0 +1,60 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace HaloDocMVC.Models
+{
+    public class DecodedToken
+    {
+        private List<string> audience1;
+        public Dictionary<string, string> claims;
+        private DateTime validTo;
+        private string signatureAlgorithm;
+        private string rawData;
+        private string subject;
+        private DateTime validFrom;
+        private string encodedHeader;
+        private string encodedPayload;
+        private string keyId;
+
+        public DecodedToken(string keyId, string issuer, List<string> audience1, Dictionary<string, string> claims, DateTime validTo, string signatureAlgorithm, string rawData, string subject, DateTime validFrom, string encodedHeader, string encodedPayload)
+        {
+            this.keyId = keyId;
+            issuer = issuer;
+            this.audience1 = audience1;
+            this.claims = claims;
+            this.validTo = validTo;
+            this.signatureAlgorithm = signatureAlgorithm;
+            this.rawData = rawData;
+            this.subject = subject;
+            this.validFrom = validFrom;
+            this.encodedHeader = encodedHeader;
+            this.encodedPayload = encodedPayload;
+        }
+        public static JwtSecurityToken ConvertJwtStringToJwtSecurityToken(string? jwt)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var token = handler.ReadJwtToken(jwt);
+
+            return token;
+        }
+        public static DecodedToken DecodeJwt(JwtSecurityToken token)
+        {
+            var keyId = token.Header.Kid;
+            var audience = token.Audiences.ToList();
+            var claims = token.Claims.ToDictionary(claim => claim.Type, claim => claim.Value);
+
+            return new DecodedToken(
+                keyId,
+                token.Issuer,
+                audience,
+                claims,
+                token.ValidTo,
+                token.SignatureAlgorithm,
+                token.RawData,
+                token.Subject,
+                token.ValidFrom,
+                token.EncodedHeader,
+                token.EncodedPayload
+            );
+        }
+    }
+}
