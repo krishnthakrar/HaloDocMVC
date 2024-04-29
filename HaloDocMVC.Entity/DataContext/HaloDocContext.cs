e643951fa18adb1e985f3a133318ae35b686f0fd
krishnthakrar <krishn.thakrar@etatvasoft.com>
2024-04-29 13:25:41 +0530

    29-04 Commit

--- HaloDocMVC.Entity/DataContext/HaloDocContext.cs
+++ HaloDocMVC.Entity/DataContext/HaloDocContext.cs
@@ -89,8 +89,9 @@ public partial class HaloDocContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("User ID = postgres;Password=Krishn@1303;Server=localhost;Port=5432;Database=HaloDoc;Integrated Security=true;Pooling=true;");
+    {
+        optionsBuilder.UseNpgsql("User ID = postgres;Password=Krishn@1303;Server=localhost;Port=5432;Database=HaloDoc;Integrated Security=true;Pooling=true;");
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
